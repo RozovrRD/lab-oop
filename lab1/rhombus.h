185dee9d2e69440d8b6ca035da61ba601d36e35b
RozovrRD <33757206+RozovrRD@users.noreply.github.com>
2017-12-22 17:18:26 +0300

    Add files via upload

---
+++ lab1/rhombus.h
@@ -0,0 +1,21 @@
+#ifndef RHOMBUS_H
+#define RHOMBUS_H
+
+#include <iostream>
+#include "Figure.h"
+
+class Rhombus : public Figure {
+public:
+	Rhombus();
+	Rhombus(std::istream& is);
+
+	void Print() override;
+	double Square() override;
+
+private:
+
+	double side;
+	double height;
+};
+
+#endif
