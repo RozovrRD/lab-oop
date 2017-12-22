bca847abc8ea73954d825d155522536434455be7
RozovrRD <33757206+RozovrRD@users.noreply.github.com>
2017-12-22 21:48:52 +0300

    Add files via upload

--- lab2/TList.cpp
+++ lab2/TList.cpp
@@ -1,169 +1,176 @@
-#include "stdafx.h"
 #include "TList.h"
+#include <iostream>
+#include <cstdint>
 
-TList::TList() :head(nullptr), size(0) {
-}
-
-std::ostream& operator<<(std::ostream& os, const TList& list) {
-	if (!list.head)
-	{
-		os << "Список пуст." << std::endl;
-	}
-	TListItem *item = list.head;
-	while (item != nullptr) {
-		os << item->GetRectangle();
-		item = item->GetNext();
-	}
-	return os;
-}
-
-void TList::Insert(Rectangle &tr) {
-	int n;
-	std::cout << "Введите индекс: ";
-	std::cin >> n;
-	if (n < 0 || n > this->GetSize()) {
-		std::cout << "Такого индекса нет.\n";
-		return;
-	}
-	if (n == 0) {
-		this->PushFirst(tr);
-	}
-	else if (n == this->GetSize() - 1) {
-		this->PushLast(tr);
-	}
-	else {
-		this->PushAtIndex(tr, n);
-	}
-	++size;
-}
-
-void TList::PushLast(Rectangle &tr)
+TList::TList()
 {
-	TListItem *newItem = new TListItem(tr);
-	TListItem *tmp = this->head;
-
-	while (tmp->GetNext() != nullptr) {
-		tmp = tmp->GetNext();
-	}
-	tmp->SetNext(newItem);
-	newItem->SetPrev(tmp);
-	newItem->SetNext(nullptr);
+    head = nullptr;
+    length = 0;
 }
 
-void TList::PushFirst(Rectangle &tr)
+void TList::Push(Rectangle &obj) {
+    int32_t index = 0;
+    std::cout << "Enter index = ";
+    std::cin >> index;
+    if (index > this->GetLength() - 1 || index < 0) {
+        std::cerr << "This index doesn't exist\n";
+        return;
+    }
+    if (index == 0) {
+        this->PushFirst(obj);
+    } else if (index == this->GetLength() - 1) {
+        this->PushLast(obj);
+    } else {
+        this->PushAtIndex(obj, index);
+    }
+    ++length;
+}
+
+void TList::PushAtIndex(Rectangle &obj, int32_t ind)
 {
-	TListItem *newItem = new TListItem(tr);
-	TListItem *oldHead = this->head;
-	this->head = newItem;
-	if (oldHead != nullptr) {
-		newItem->SetNext(oldHead);
-		oldHead->SetPrev(newItem);
-	}
+    TListItem *newItem = new TListItem(obj);
+    TListItem *tmp = this->head;
+    for(int32_t i = 1; i < ind; ++i){
+        tmp = tmp->GetNext();
+    }
+    newItem->SetNext(tmp->GetNext());
+    newItem->SetPrev(tmp);
+    tmp->SetNext(newItem);
+    tmp->GetNext()->SetPrev(newItem);
 }
 
-
-void TList::PushAtIndex(Rectangle &triangle, int n) {
-	TListItem *p = new TListItem(triangle);
-	TListItem *tmp = this->head;
-	for (int i = 1; i < n; i++) {
-		tmp = tmp->GetNext();
-	}
-	p->SetNext(tmp->GetNext());
-	p->SetPrev(tmp);
-	tmp->SetNext(p);
-	tmp->GetNext()->SetPrev(p);
-}
-
-
-Rectangle TList::Delete()
+void TList::PushLast(Rectangle &obj)
 {
-	int n = 0;
-	Rectangle rec;
-	std::cout << "Введите индекс: ";
-	std::cin >> n;
-	if (n > this->GetSize() - 1 || n < 0 || this->IsEmpty()) {
-		std::cout << "Неверный ввод.\n";
-		return rec;
-	}
-	if (n == 0) {
-		rec = this->PopFirst();
-	}
-	else if (n == this->GetSize() - 1) {
-		rec = this->PopLast();
-	}
-	else {
-		rec = this->PopAtIndex(n);
-	}
-	--size;
-	return rec;
+    TListItem *newItem = new TListItem(obj);
+    TListItem *tmp = this->head;
+
+    while (tmp->GetNext() != nullptr) {
+        tmp = tmp->GetNext();
+    }
+    tmp->SetNext(newItem);
+    newItem->SetPrev(tmp);
+    newItem->SetNext(nullptr);
 }
 
-Rectangle TList::PopAtIndex(int n)
+void TList::PushFirst(Rectangle &obj)
 {
-	TListItem *tmp = this->head;
-	for (int i = 0; i < n - 1; ++i) {
-		tmp = tmp->GetNext();
-	}
-	TListItem *rem = tmp->GetNext();
-	Rectangle res = rem->GetRectangle();
-	TListItem *nextItem = rem->GetNext();
-	tmp->SetNext(nextItem);
-	nextItem->SetPrev(tmp);
-	delete rem;
-	return res;
+    TListItem *newItem = new TListItem(obj);
+    TListItem *oldHead = this->head;
+    this->head = newItem;
+    if(oldHead != nullptr) {
+        newItem->SetNext(oldHead);
+        oldHead->SetPrev(newItem);
+    }
 }
+
+uint32_t TList::GetLength()
+{
+    return this->length;
+}
+
+const bool TList::IsEmpty() const
+{
+    return head == nullptr;
+}
+
+Rectangle TList::Pop()
+{
+    int32_t ind = 0;
+    std::cout << "Enter index = ";
+    std::cin >> ind;
+    Rectangle res;
+    if (ind > this->GetLength() - 1 || ind < 0 || this->IsEmpty()) {
+        std::cout << "Change index" << std::endl;
+        return res;
+    }
+
+    if (ind == 0) {
+        res = this->PopFirst();
+    } else if (ind == this->GetLength() - 1) {
+        res = this->PopLast();
+    } else {
+        res = this->PopAtIndex(ind);
+    }
+    --length;
+    return res;
+}
+
+Rectangle TList::PopAtIndex(int32_t ind)
+{
+    TListItem *tmp = this->head;
+    for(int32_t i = 0; i < ind - 1; ++i) {
+        tmp = tmp->GetNext();
+    }
+    TListItem *removed = tmp->GetNext();
+    Rectangle res = removed->GetFigure();
+    TListItem *nextItem = removed->GetNext();
+    tmp->SetNext(nextItem);
+    nextItem->SetPrev(tmp);
+    delete removed;
+    return res;
+}
+
 
 Rectangle TList::PopFirst()
 {
-	if (this->GetSize() == 1) {
-		Rectangle res = this->head->GetRectangle();
-		delete this->head;
-		this->head = nullptr;
-		return res;
-	}
-	TListItem *tmp = this->head;
-	Rectangle res = tmp->GetRectangle();
-	this->head = this->head->GetNext();
-	this->head->SetPrev(nullptr);
-	delete tmp;
-	return res;
+    if (this->GetLength() == 1) {
+        Rectangle res = this->head->GetFigure();
+        delete this->head;
+        this->head = nullptr;
+        return res;
+    }
+    TListItem *tmp = this->head;
+    Rectangle res = tmp->GetFigure();
+    this->head = this->head->GetNext();
+    this->head->SetPrev(nullptr);
+    delete tmp;
+    return res;
 }
 
 Rectangle TList::PopLast()
 {
-	if (this->GetSize() == 1) {
-		Rectangle res = this->head->GetRectangle();
-		delete this->head;
-		this->head = nullptr;
-		return res;
-	}
-	TListItem *tmp = this->head;
-	while (tmp->GetNext()->GetNext()) {
-		tmp = tmp->GetNext();
-	}
-	TListItem *rem = tmp->GetNext();
-	Rectangle res = rem->GetRectangle();
-	tmp->SetNext(rem->GetNext());
-	delete rem;
-	return res;
+    if (this->GetLength() == 1) {
+        Rectangle res = this->head->GetFigure();
+        delete this->head;
+        this->head = nullptr;
+        return res;
+    }
+    TListItem *tmp = this->head;
+    while(tmp->GetNext()->GetNext()) {
+        tmp = tmp->GetNext();
+    }
+    TListItem *removed = tmp->GetNext();
+    Rectangle res = removed->GetFigure();
+    tmp->SetNext(removed->GetNext());
+    delete removed;
+    return res;
 }
 
-int TList::GetSize()
+
+std::ostream& operator<<(std::ostream &os, const TList &list)
 {
-	return this->size;
+    if (list.IsEmpty()) {
+        os << "The list is empty." << std::endl;
+        return os;
+    }
+
+    TListItem *tmp = list.head;
+    for(int32_t i = 0; tmp; ++i) {
+        os << "idx: " << i << "   ";
+        os << *tmp << std::endl;
+        tmp = tmp->GetNext();
+    }
+
+    return os;
 }
 
-bool TList::IsEmpty() const
+TList::~TList()
 {
-	return head == nullptr;
-}
-
-TList::~TList() {
-	TListItem* tmp;
-	while (head) {
-		tmp = head;
-		head = head->GetNext();
-		delete tmp;
-	}
+    TListItem *tmp;
+    while (head) {
+        tmp = head;
+        head = head->GetNext();
+        delete tmp;
+    }
 }
 
